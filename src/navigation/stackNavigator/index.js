2264ffe1a781a8e6a21281aa05ce8aec7af2abac
santisilva <37660894+santisilva@users.noreply.github.com>
2022-02-11 08:56:25 -0300

    config inicial

---
+++ src/navigation/stackNavigator/index.js
@@ -0,0 +1,49 @@
+import React, {useContext, useEffect, useState} from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  Login
+} from '../../screens';
+
+
+const StackNav = createStackNavigator();
+
+/**
+ * @returns Main Stack navigator for the app
+ */
+
+const StackNavigator = () => {
+  
+
+  return (
+      <NavigationContainer theme={theme}>
+        <StackNav.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#012f75',
+              height: 70,
+            },
+            headerBackTitleStyle: {color: '#fff'},
+            headerTitleContainerStyle: {
+              paddingVertical: 10,
+              justifyContent: 'center',
+            },
+            headerTintColor: '#fff',
+            // eslint-disable-next-line react/display-name
+            headerBackTitle: 'Atras',
+            cardStyle: {backgroundColor: '#fff'},
+          }}
+        >
+            <>
+              <StackNav.Screen
+                name={'Login'}
+                options={{headerShown: false}}
+                component={Login}
+              />
+            </>
+        </StackNav.Navigator>
+      </NavigationContainer>
+  );
+};
+
+export default StackNavigator;
