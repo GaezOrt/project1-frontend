1409b13c3d19873047b7f5a63a7bfa3d65989b7a
Gaezort <gaezort@gmail.com>
2022-04-12 22:21:42 -0300

    agrego

--- src/screens/index-screen/index.js
+++ src/screens/index-screen/index.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  FlatList,
+  ScrollView,
+} from 'react-native';
+import axios from 'axios';
 
 const Index = () => {
+  const getData = () => {
+    axios.get(`http://10.0.2.2:3000/clients`).then(res => {
+      console.log(res.data);
+      setListData(res.data);
+    });
+  };
+  const [listData, setListData] = useState(null);
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <View>
-      <Text style={styles.textColor}>Index</Text>
-      <Text>Index</Text>
-    </View>
+    <ScrollView>
+      <FlatList
+        
+        data={listData}
+        renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
+      />
+    </ScrollView>
   );
 };
 
 export default Index;
 
 const styles = StyleSheet.create({
-  screenContainer: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
-    padding: 16,
+    marginTop: StatusBar.currentHeight || 0,
   },
-  textColor: {
-    color: 'red',
-    fontSize: 50,
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
